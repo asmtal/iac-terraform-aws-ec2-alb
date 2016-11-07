0dd823e0515cd9367ffe2f654e4ce8edc8ae4a33
Georgios Fysaris <gfisairs@gmail.com>
2016-11-07 20:33:42 +0000

    Adding new AWS EC2 ALB Listener: HTTP-80

--- alb-listeners/HTTP-80.tf
+++ alb-listeners/HTTP-80.tf
@@ -1,17 +1,17 @@
-resource "aws_alb_listener" "myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL_myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT" {
-  port      = "myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT"
-  protocol  = "myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL"
-  load_balancer_arn = "${aws_alb.myapp-51054880c6b1945a14e07ff2d1f46990444b6e39.arn}"
+resource "aws_alb_listener" "myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL_myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT" {
+  port      = "myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT"
+  protocol  = "myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL"
+  load_balancer_arn = "${aws_alb.myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc.arn}"
 
   default_action {
     type              = "forward"
-    target_group_arn  = "${aws_alb_target_group.myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_TG.arn}"
+    target_group_arn  = "${aws_alb_target_group.myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_TG.arn}"
   }
 }
-  output "lst-myapp-51054880c6b1945a14e07ff2d1f46990444b6e39-myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL_myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT-id" {
-    value = "${aws_alb_listener.myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL_myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT.id}"
+  output "lst-myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc-myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL_myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT-id" {
+    value = "${aws_alb_listener.myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL_myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT.id}"
   }
 
-  output "lst-myapp-51054880c6b1945a14e07ff2d1f46990444b6e39-myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL_myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT-arn" {
-    value = "${aws_alb_listener.myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRTCL_myapp-51054880c6b1945a14e07ff2d1f46990444b6e39_LST_PRT.arn}"
+  output "lst-myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc-myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL_myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT-arn" {
+    value = "${aws_alb_listener.myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRTCL_myapp-ddbdb0f3edda707128cd968730e911cf47eeabcc_LST_PRT.arn}"
   }
